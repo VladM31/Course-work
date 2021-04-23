b0e2ec37af551fe78b89297d019d15afea0999e8
VladM31 <83096749+VladM31@users.noreply.github.com>
2021-04-24 01:34:36 +0300

    Делаю тестовий интерфей роботи с файлами

--- Курсач/Курсач.cpp
+++ Курсач/Курсач.cpp
@@ -5,12 +5,24 @@ using namespace std;
 
 int main() {
     Ukr;
-    Grup<string> *NameFile= new Grup<string>(FindFileGrup());
-    Grup<ifstream*> OpenFile;
+    string temp;
+    Grup<string> *NameFile= new Grup<string>(FindFileGrup());// Создаю лист названий файлов груп
+    Grup<ifstream*> OpenFile;//Создаю лист для открития файлов
+    Grup<Grup<Student*>*> ListGrup;
     for (size_t i = 0; i < NameFile->GetSize(); i++)
     { 
+        ListGrup.push_back(new Grup<Student*>);
         OpenFile.push_back(new ifstream((*NameFile)[i].c_str()));
-    } 
+        cout << "Група " << (*NameFile)[i] << " Дипломники?" << endl
+            << "Введіть true or false : "; cin >> temp;
+        while (!OpenFile[i]->eof())
+        {
+            if (temp=="true")
+            {
 
+            }
+        }
+    } 
+    
     Pause_Use; 
 }
